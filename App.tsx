24fe8d951c92643ed556c1a025a95822cc7c2218
iosoicristiana <niculitacristiana@gmail.com>
2024-04-12 22:54:06 +0300

    navigation

---
+++ App.tsx
@@ -0,0 +1,23 @@
+
+import { StyleSheet, Text, View } from "react-native";
+import {SafeAreaView} from 'react-native-safe-area-context';
+import Router from './src/router';
+import 'react-native-gesture-handler';
+import { AuthContextProvider } from "./src/hooks/authContext";
+
+export default function App() {
+  return (
+    <AuthContextProvider>  
+      <Router/>
+    </AuthContextProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ src/screens/game/Table.screen.tsx
@@ -0,0 +1,9 @@
+import { NavigationProp, useNavigation } from "@react-navigation/native"
+import { useAuth } from "../../hooks/authContext"
+import { Text } from "react-native"
+
+const TableScreen = () => {
+    return <Text>Game</Text>
+}
+
+export default TableScreen
