61e55c8632f8ed36b7f09edc0c20f780cd640c37
Diana Muscalu <93083024+Diana1407@users.noreply.github.com>
2024-04-13 02:49:16 +0300

    Lobby

--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,7 @@
-const baseUrl = 'http://163.172.177.98:8081';
-// const baseUrl = 'https://malamute-enabled-yak.ngrok-free.app'
+//import { CellId } from "./hooks/gameContext";
 
+const baseUrl = 'http://163.172.177.98:8081';
+console.log(baseUrl)
 const baseHeaders = {
     "Content-Type": 'application/json',
     "Accept": 'application/json'
@@ -19,7 +20,7 @@ export const login = async (email: string, password: string): Promise<string> =>
 
     const data = await result.json()
 
-    console.log(data)
+    console.log(data);
 
     return data.accessToken
 };
@@ -37,7 +38,64 @@ export const register = async (email: string, password: string) => {
 
     const data = await result.json()
 
-    console.log(data)
+    console.log(data);
 
     return data.accessToken
 };
+
+export const listGames = async (token: string) => {
+    const result = await fetch(`${baseUrl}/game`, {
+        method: 'GET',
+        headers: {
+            ...baseHeaders,
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const data = await result.json();
+    return data.games
+}
+
+export const createGame = async (token: string) => {
+    const result = await fetch(`${baseUrl}/game`, {
+        method: 'POST',
+        headers: {
+            ...baseHeaders,
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const data = await result.json();
+    return data
+}
+
+export const loadGame = async (token: string, gameId: number) => {
+    const result = await fetch(`${baseUrl}/game/${gameId}`, {
+        method: 'GET',
+        headers: {
+            ...baseHeaders,
+            'Authorization': `Bearer ${token}`
+        }
+    })
+
+    const data = await result.json();
+
+    return data
+}
+
+// export const sendMove = async (token: string, gameId: number, cell: CellId) => {
+//     const result = await fetch(`${baseUrl}/game/move/${gameId}`, {
+//         method: 'POST',
+//         headers: {
+//             ...baseHeaders,
+//             'Authorization': `Bearer ${token}`
+//         },
+//         body: JSON.stringify({
+//             cell
+//         })
+//     })
+
+//     const data = await result.json();
+
+//     return data
+// }
--- src/hooks/authContext.tsx
+++ src/hooks/authContext.tsx
@@ -1,50 +1,73 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { login, register } from "../api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface IAuthContext {
-    token: string;
-    login: (email: string, password: string) => Promise<void>;
-    register: (email: string, password: string) => Promise<void>;
+  token: string;
+  login: (email: string, password: string) => Promise<void>;
+  register: (email: string, password: string) => Promise<void>;
+  isLoading: boolean;
 }
 
 const AuthContext = createContext<IAuthContext>({
-    token: '',
-    login: async () => {},
-    register: async () => {}
-})
+  token: "",
+  login: async () => {},
+  register: async () => {},
+  isLoading: false,
+});
 
-export const AuthContextProvider: React.FC<{children: React.ReactNode}> = ({children}) => {
+export const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  const [token, setToken] = useState<string>("");
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [token, setToken] = useState<string>('');
-
-    const handleLogin = async (email: string, password: string) => {
-        try {
-            const result = await login(email, password);
-            console.log('login: ', result)
-            setToken(result);
-        } catch (error) {
-            console.log(error)
+  useEffect(() => {
+    setIsLoading(true);
+    AsyncStorage.getItem("token")
+      .then((value) => {
+        if (value !== null) {
+          setToken(value);
         }
-    };
-    const handleRegister = async (email: string, password: string) => {
-        try {
-            const result = await register(email, password);
-            console.log('register: ', result)
-            setToken(result);
-        } catch (error) {
-            console.log(error)
-        }
-    };
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }, []);
 
-    return (
-        <AuthContext.Provider value={{
-            token,
-            login: handleLogin,
-            register: handleRegister
-        }}>
-            {children}
-        </AuthContext.Provider>
-    )
+  const handleLogin = async (email: string, password: string) => {
+    try {
+      const result = await login(email, password);
+      console.log("login: ", result);
+      setToken(result);
+      await AsyncStorage.setItem("token", result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleRegister = async (email: string, password: string) => {
+    try {
+      const result = await register(email, password);
+      console.log("register: ", result);
+      setToken(result);
+      await AsyncStorage.setItem("token", result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        token,
+        login: handleLogin,
+        register: handleRegister,
+        isLoading,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => useContext(AuthContext);
